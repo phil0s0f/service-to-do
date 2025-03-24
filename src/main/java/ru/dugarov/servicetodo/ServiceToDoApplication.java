08024e74fb2b328cefd7568133edc6c18d5216de
Дугаров Вячеслав <sdugharow@yandex.ru>
2025-03-24 19:31:16 +0800

    initial commit

---
+++ src/main/java/ru/dugarov/servicetodo/ServiceToDoApplication.java
@@ -0,0 +1,13 @@
+package ru.dugarov.servicetodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceToDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dugarov/servicetodo/ServiceToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dugarov.servicetodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
